b9b1102230ac04bd40df288ed998cc3dd6b0dc91
weverton fagner <wevertoonfagneeer@outlook.com.br>
2022-01-14 16:09:42 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio de codigo DIO - Calculo simples/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Desafio_de_codigo_DIO___Calculo_simples
+{
+    class DIO
+    {
+        static void Main(string[] args)
+        {
+            string[] input;
+            input = Console.ReadLine().Split(' ');
+            int cod1 = int.Parse(input[0]);
+            int n1 = int.Parse(input[1]);
+            double valor1 = double.Parse(input[2]);
+
+            input = Console.ReadLine().Split(' ');
+            int cod2 = int.Parse(input[0]);
+            int n2 = int.Parse(input[1]);
+            double valor2 = double.Parse(input[2]);
+
+            double total = (n1 * valor1) + (n2 * valor2);
+            Console.WriteLine("VALOR A PAGAR: R$ {0:F2}", total);
+        }
+    }
+}
